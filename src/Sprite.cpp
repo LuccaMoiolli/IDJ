a6f7a71affe6852ebfa87c0e20703e4c26b942fc
Lucca Moiolli <74780608+LuccaMoiolli@users.noreply.github.com>
2024-10-23 13:29:56 -0300

    Add files via upload

--- src/Sprite.cpp
+++ src/Sprite.cpp
@@ -22,16 +22,35 @@ class Sprite{
         void Open(std::string file){
             if(texture == nullptr)
                 SDL_DestroyTexture(texture);
+            
             try{
                 IMG_LoadTexture(game.GetRenderer(), file.c_str()); // como usa game aqui?
+            }catch(...){
+                SDL_Log(SDL_GetError());
+                SDL_Log("Falha ao carregar imagem");
             }
+
+            SDL_QueryTexture(texture, nullptr, nullptr, &width, &height);
+            SetClip(0, 0, width, height); // 0s?
         }
         
-        void SetClip(int x, int y, int w, int h);
-        void Render(int x, int y);
-        int GetWidth();
-        int GetHeight();
-        bool IsOpen();
+        void SetClip(int x, int y, int w, int h){
+            clipRect.x = x;
+            clipRect.y = y;
+            clipRect.w = w;
+            clipRect.h = h;
+        }
+
+        void Render(int x, int y){
+            SDL_Rect tmp(clipRect);
+            tmp.x = x; tmp.y = y;
+            SDL_RenderCopy(game.GetRenderer(), texture, &clipRect, &tmp); // game dnv
+        }
+
+        int GetWidth(){ return width;}
+        int GetHeight(){ return height;}
+        bool IsOpen(){ return texture != nullptr;}
+
     private:
         SDL_Texture* texture;
         int width;
